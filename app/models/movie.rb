6c5905bb344f13534c6ef8a8170ffc5af0840fa2
Yuri Santana <yuri03ysl@gmail.com>
2026-04-13 16:40:47 -0300

    refactoring

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  RATINGS = %w[G PG PG-13 R NC-17].freeze
+
+  validates :title, presence: true
+  validates :rating, inclusion: { in: RATINGS, message: 'must be G, PG, PG-13, R, or NC-17' }, allow_blank: true
 end
